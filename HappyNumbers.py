1411a2601d5fe79ebcd1b3486ab7a8e245ea4693
Paulo César <paulocesarcs.dev@gmail.com>
2016-12-31 19:08:27 -0200

    adicionado recursividade

--- HappyNumbers.py
+++ HappyNumbers.py
@@ -3,7 +3,11 @@ def sum_of_squares(number):
     digits = [int(char) ** 2 for char in string]
     return sum(digits)
 
+
 def happy(number):
+        if number < 10:
+            return number in (1, 7)
+        return happy(sum_of_squares(number))
 
         box = []
         n = number
@@ -14,9 +18,5 @@ def happy(number):
 
 
 assert sum_of_squares(130) == 10
-assert happy(1)
-assert happy(10)
-assert happy(100)
-assert happy(130)
-assert happy(97)
-assert not happy(4)
+assert all([happy(n)for n in (1, 10, 100, 130, 97)])
+assert not all(happy(n) for n in (2, 3, 4, 5, 6, 8, 9))
