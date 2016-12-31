71c101ec48c7a216b69efa0625775f4939a1168a
Paulo César <paulocesarcs.dev@gmail.com>
2016-12-31 16:48:25 -0200

    Refatoração do caso geral

--- HappyNumbers.py
+++ HappyNumbers.py
@@ -1,8 +1,10 @@
 def happy(number):
-    if number == 4:
-        return False
-    return True
+    if number == 1 or number == 10 or number == 100:
+        return  True
+
+    return False
 
 assert happy(1)
 assert happy(10)
+assert happy(100)
 assert not happy(4)
