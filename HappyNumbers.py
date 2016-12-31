d2dcfe7b55ab5f01a3c2ca9bf3dad6cd3fb07c23
Paulo César <paulocesarcs.dev@gmail.com>
2016-12-31 19:09:08 -0200

    adicionado recusividade e apaguei algumas linhas que esqueci

--- HappyNumbers.py
+++ HappyNumbers.py
@@ -9,14 +9,6 @@ def happy(number):
             return number in (1, 7)
         return happy(sum_of_squares(number))
 
-        box = []
-        n = number
-        while n != 1 and n not in box:
-            box.append(n)
-            n = sum_of_squares(n)
-        return n == 1
-
-
 assert sum_of_squares(130) == 10
 assert all([happy(n)for n in (1, 10, 100, 130, 97)])
 assert not all(happy(n) for n in (2, 3, 4, 5, 6, 8, 9))
