c50f7a3d923ff3f62b805513c7a4b4965979b745
Paulo César <paulocesarcs.dev@gmail.com>
2016-12-31 18:35:14 -0200

    adicionado a função do 130

--- HappyNumbers.py
+++ HappyNumbers.py
@@ -1,6 +1,12 @@
+def sum_of_digits(number):
+    string = str(number)
+    digits = [int(char) ** 2 for char in string]
+    return sum(digits)
+
 def happy(number):
     if number == 130:
         return True
+
     if number in (1, 10, 100):
         string = str(number)
         digits = [int (char) for char in string]
@@ -9,7 +15,7 @@ def happy(number):
         return total == 1
 
     return False
-
+assert sum_of_digits(130) == 10
 assert happy(1)
 assert happy(10)
 assert happy(100)
