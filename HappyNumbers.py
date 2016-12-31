a314c7288f33f5df02190b4977b9cb95d4cb2945
Paulo César <paulocesarcs.dev@gmail.com>
2016-12-31 18:45:00 -0200

    refatoração os ifs

--- HappyNumbers.py
+++ HappyNumbers.py
@@ -1,21 +1,21 @@
-def sum_of_digits(number):
+def sum_of_squares(number):
     string = str(number)
     digits = [int(char) ** 2 for char in string]
     return sum(digits)
 
 def happy(number):
     if number == 130:
-        return True
+        n = number
+        n = sum_of_squares(n)
+        n = sum_of_squares(n)
+        return n == 1
 
     if number in (1, 10, 100):
-        string = str(number)
-        digits = [int (char) for char in string]
-        total = sum(digits)
-
+        total = sum_of_squares(number)
         return total == 1
 
     return False
-assert sum_of_digits(130) == 10
+assert sum_of_squares(130) == 10
 assert happy(1)
 assert happy(10)
 assert happy(100)
