6f21552e4dd0d2aa0d410487e44280a27f18c436
Paulo César <paulocesarcs.dev@gmail.com>
2016-12-31 19:14:33 -0200

    refatoração final

--- HappyNumbers.py
+++ HappyNumbers.py
@@ -1,13 +1,10 @@
 def sum_of_squares(number):
-    string = str(number)
-    digits = [int(char) ** 2 for char in string]
-    return sum(digits)
+    return sum(int(char) ** 2 for char in str(number))
 
 
 def happy(number):
-        if number < 10:
-            return number in (1, 7)
-        return happy(sum_of_squares(number))
+        next_ = sum(int(char) ** 2 for char in str(number))
+        return  number in (1, 7) if number < 10 else  happy(next_)
 
 assert sum_of_squares(130) == 10
 assert all([happy(n)for n in (1, 10, 100, 130, 97)])
